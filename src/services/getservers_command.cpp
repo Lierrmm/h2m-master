775799a9112b83f9292be1b318c3480c57d777b1
Louve <33836535+Rackover@users.noreply.github.com>
2023-02-20 12:29:14 +0100

    send multiple responses if the server list size exceeds MTU (#443)
    Co-authored-by: Louvenarde <louve@louve.systems>
    Co-authored-by: Edo <edoardo.sanguineti222@gmail.com>

--- src/services/getservers_command.cpp
+++ src/services/getservers_command.cpp
@@ -5,6 +5,23 @@
 
 #include <utils/parameters.hpp>
 
+namespace
+{
+	struct prepared_server
+	{
+		uint32_t address;
+		uint16_t port;
+	};
+}
+
+constexpr auto MTU = 1400; // Real UDP MTU is more like 1500 bytes, but we keep a little wiggle room just in case
+constexpr auto DPM_PROTOCOL_ADDRESS_LENGTH = sizeof prepared_server::address;
+constexpr auto DPM_PROTOCOL_PORT_LENGTH = sizeof prepared_server::port;
+
+static_assert(DPM_PROTOCOL_ADDRESS_LENGTH == 4);
+static_assert(DPM_PROTOCOL_PORT_LENGTH == 2);
+
+
 const char* getservers_command::get_command() const
 {
 	return "getservers";
@@ -31,33 +48,47 @@ void getservers_command::handle_command(const network::address& target, const st
 	const auto game_type = resolve_game_type(game);
 	if (game_type == game_type::unknown)
 	{
-		throw execution_exception{"Invalid game type: " + game};
+		throw execution_exception{ "Invalid game type: " + game };
 	}
 
-	auto count = 0;
-	std::string response{};
+	std::queue<prepared_server> prepared_servers{};
 
 	this->get_server().get_server_list() //
-	    .find_registered_servers(game_type, protocol,
-	                             [&response, &count](const game_server&, const network::address& address)
-	                             {
-		                             const auto addr = address.get_in_addr().sin_addr.s_addr;
-		                             const auto port = htons(address.get_port());
+		.find_registered_servers(game_type, protocol,
+			[&prepared_servers](const game_server&, const network::address& address)
+			{
+				const auto addr = address.get_in_addr().sin_addr.s_addr;
+				const auto port = htons(address.get_port());
 
-		                             response.push_back('\\');
-		                             response.append(reinterpret_cast<const char*>(&addr), 4);
-		                             response.append(reinterpret_cast<const char*>(&port), 2);
+				prepared_servers.push({ addr, port });
+			});
 
-		                             ++count;
-	                             });
+	size_t packet_count = 0;
 
-	response.push_back('\\');
-	response.append("EOT");
-	response.push_back('\0');
-	response.push_back('\0');
-	response.push_back('\0');
+	std::string response{};
 
-	this->get_server().send(target, "getserversResponse", response);
+	while (!prepared_servers.empty())
+	{
+		const auto& server = prepared_servers.front();
+		response.push_back('\\');
+		response.append(reinterpret_cast<const char*>(&server.address), DPM_PROTOCOL_ADDRESS_LENGTH);
+		response.append(reinterpret_cast<const char*>(&server.port), DPM_PROTOCOL_PORT_LENGTH);
+		prepared_servers.pop();
 
-	console::log("Sent %d servers for game %s:\t%s", count, game.data(), target.to_string().data());
+		if (response.size() >= MTU || prepared_servers.empty())
+		{
+			response.push_back('\\');
+			response.append("EOT");
+			response.push_back('\0');
+			response.push_back('\0');
+			response.push_back('\0');
+
+			this->get_server().send(target, "getserversResponse", response);
+			packet_count++;
+
+			response.clear();
+		}
+	}
+
+	console::log("Sent %zu servers in %zu parts for game %s:\t%s", prepared_servers.size(), packet_count, game.data(), target.to_string().data());
 }
